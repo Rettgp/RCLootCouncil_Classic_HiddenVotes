ee94a046e31651db977b3bbae2029e5f6f882dc8
Garrett <garephelps@live.com>
2023-01-18 20:31:10 -0600

    Fix curse ID

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -13,7 +13,7 @@ mkdir "$curDir/.tmp/"
 mkdir "$curDir/.tmp/RCLootCouncil_Classic_HiddenVotes"
 
 # Build RCLootCouncil (locale only) (and skip Libs as they shouldn't be processed with the keyword replacements)
-( bash "$release_script" -dLsz -t "$curDir/RCLootCouncil" -r "$curDir/.tmp/RCLootCouncil_Classic_HiddenVotes" -p 39928 -m ".pkgmeta-rclootcouncil" )
+( bash "$release_script" -dLsz -t "$curDir/RCLootCouncil" -r "$curDir/.tmp/RCLootCouncil_Classic_HiddenVotes" -p 813192 -m ".pkgmeta-rclootcouncil" )
 # Now just copy the original libs to the build
 cp -R "$curDir/RCLootCouncil/Libs/" "$curDir/.tmp/RCLootCouncil_Classic_HiddenVotes/RCLootCouncil/"
 
