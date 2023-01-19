106b041b81d0b5c89cef3584b023c2de37fa7d92
Garrett <garephelps@live.com>
2023-01-18 21:33:51 -0600

    Override SetCellVotes so only officers and GM can see the vote count

--- API/VotingFrameUpdates.lua
+++ API/VotingFrameUpdates.lua
@@ -1,6 +1,6 @@
 local _, addon = ...
 local VotingFrame = addon:GetModule("RCVotingFrame")
-
+local L = LibStub("AceLocale-3.0"):GetLocale("RCLootCouncil")
 
 function VotingFrame:UpdateLootStatus ()
    -- Do nothing
@@ -14,4 +14,39 @@ function VotingFrame.SetCellClass(rowFrame, frame, data, cols, row, realrow, col
    local class = VotingFrame:GetCandidateData(1, name, "class")
 	addon.SetCellClassIcon(rowFrame, frame, data, cols, row, realrow, column, fShow, table, class)
 	data[realrow].cols[column].value = class or ""
+end
+
+function VotingFrame.SetCellVotes(rowFrame, frame, data, cols, row, realrow, column, fShow, table, ...)
+	local name = data[realrow].name
+	frame:SetScript("OnEnter", function()
+         local voters = VotingFrame:GetCandidateData(VotingFrame:GetCurrentSession(), name, "voters")
+
+         -- for i = 1, GetNumGuildMembers() do
+         --    local name, _, rankIndex = GetGuildRosterInfo(i) -- get info from all guild members
+         --    if rankIndex + 1 <= val then -- if the member is the required rank, or above
+         --       tinsert(self.db.profile.council, name) -- then insert them to the council
+         --    end
+         -- end
+		if not addon.mldb.anonymousVoting or (db.showForML and addon.isMasterLooter) then
+				addon:CreateTooltip(L["Voters"], unpack((function ()
+					local ret = {}
+					for i,name in ipairs(voters) do
+						ret[i] = addon:GetUnitClassColoredName(name)
+					end
+					return ret
+				end)()
+			))
+		end
+	end)
+   local votes = VotingFrame:GetCandidateData(VotingFrame:GetCurrentSession(), name, "votes")
+	frame:SetScript("OnLeave", function() addon:HideTooltip() end)
+   local rank_name = addon.guildRank
+   local guild_ranks = addon:GetGuildRanks()
+   if guild_ranks and guild_ranks[rank_name] > 4 then
+      data[realrow].cols[column].value = votes -- Set the value for sorting reasons
+      frame.text:SetText(votes)
+   else
+      frame.text:SetText("?")
+      data[realrow].cols[column].value = 0 -- Don't background sort when we can't see the votes
+   end
 end
